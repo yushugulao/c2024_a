49fed1d3da128b91a556dc2cc39d766a2991680c
yushugulao <mianbaogougou@163.com>
2025-01-02 21:49:22 +0800

    总算弄好了

---
+++ gobangWithKlee/此文件夹内为可运行程序/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_gobangForAssignment_FILE C:/Users/HONOR/Documents/gobangForAssignment/build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/gobangForAssignment.exe)
+set(__QT_DEPLOY_TARGET_gobangForAssignment_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_gobangForAssignment_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
